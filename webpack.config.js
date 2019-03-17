1baf774eaad2ee03d8363720c12147b9ccf7769d
Ravshan <ravshansbox@gmail.com>
2019-03-17 14:05:45 +0500

    add normal and minified output to build

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'browser-crypto.js',
     library: 'browserCrypto',
     libraryTarget: 'umd',
     globalObject: 'this'
