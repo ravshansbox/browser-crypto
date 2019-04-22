323d5a6c9e37aa383fcaa3c4c0774b1b8a85bdf0
Ravshan Samandarov <ravshansbox@gmail.com>
2019-04-22 18:10:41 +0500

    update 'export from ...' to 'import from ...'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Buffer as _Buffer } from 'safe-buffer';
 import _createECDH from 'create-ecdh/browser';
-export {
+import {
   getCiphers as _getCiphers,
   createCipher as _createCipher,
   createCipheriv as _createCipheriv,
@@ -9,13 +9,12 @@ export {
 } from 'browserify-aes/browser';
 
 export const Buffer = _Buffer;
-export const getCiphers = _getCiphers;
 export const createECDH = _createECDH;
+export const getCiphers = _getCiphers;
 export const createCipher = _createCipher;
 export const createCipheriv = _createCipheriv;
 export const createDecipher = _createDecipher;
 export const createDecipheriv = _createDecipheriv;
-
 export default {
   Buffer,
   getCiphers,
